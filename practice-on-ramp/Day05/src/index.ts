e849351a5b4101f144c78c4a300ec770c42b5aab
Krantdarshi2023 <krantd@mobiquityinc.com>
2023-06-15 15:38:14 +0530

    Day05 work

---
+++ practice-on-ramp/Day05/src/index.ts
@@ -0,0 +1,19 @@
+class Car{
+    colour: string;
+    constructor( colour:string){
+        this.colour=colour;
+    }
+}
+class Child extends Car{
+    price:number;
+    constructor(colour:string,price:number){
+        super(colour)
+        this.price=price;
+    }
+    display=()=>{
+       return `Colour of car is ${this.colour} and Price is ${this.price}`;
+    }
+}
+
+const car1=new Child("Red",20000);
+console.log(car1.display());
